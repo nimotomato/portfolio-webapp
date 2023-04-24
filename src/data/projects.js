39f35d0bef6c5b7daa0c0e16e0808362a52276f0
Nimotomato <108477753+nimotomato@users.noreply.github.com>
2023-04-24 19:50:01 +0200

    changed link IIGTH

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,33 +1,33 @@
-import github from '../img/github.jpg';
-import IITH from '../img/IITH.jpg';
-import pong from '../img/pong.jpg';
-import spotifyBot from '../img/spotifyBot.jpg';
+import github from "../img/github.jpg";
+import IITH from "../img/IITH.jpg";
+import pong from "../img/pong.jpg";
+import spotifyBot from "../img/spotifyBot.jpg";
 
 const projectList = [
-    { 
-      url: "https://isitgoingtohell-lzrwjhdvn-nimotomato.vercel.app/",
-      img: IITH,
-      text: "Sentiment analysis",
-      altImg: github, 
-      altUrl: "https://github.com/nimotomato/isitgoingtohell-webpage-portfolio",
-      id: 1
-    }, 
-    { 
-      url: "https://open.spotify.com/playlist/2iz1IxBJVzcmnOrPDxQfYj?si=2eef6e14a3304d56",
-      img: spotifyBot,
-      text: "Spotify bot",
-      altImg: github, 
-      altUrl: "https://github.com/nimotomato/DiscoverLongly",
-      id: 2
-    }, 
-    { 
-      url: "https://pong-ten-plum.vercel.app/",
-      img: pong,
-      text: "Pong single player",
-      altImg: github, 
-      altUrl: "https://github.com/nimotomato/pong-portfolio",
-      id: 3
-    }  
-  ];
+  {
+    url: "https://isitgoingtohell.vercel.app/",
+    img: IITH,
+    text: "Sentiment analysis",
+    altImg: github,
+    altUrl: "https://github.com/nimotomato/isitgoingtohell-webpage-portfolio",
+    id: 1,
+  },
+  {
+    url: "https://open.spotify.com/playlist/2iz1IxBJVzcmnOrPDxQfYj?si=2eef6e14a3304d56",
+    img: spotifyBot,
+    text: "Spotify bot",
+    altImg: github,
+    altUrl: "https://github.com/nimotomato/DiscoverLongly",
+    id: 2,
+  },
+  {
+    url: "https://pong-ten-plum.vercel.app/",
+    img: pong,
+    text: "Pong single player",
+    altImg: github,
+    altUrl: "https://github.com/nimotomato/pong-portfolio",
+    id: 3,
+  },
+];
 
 export default projectList;
