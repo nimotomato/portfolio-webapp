467b2437d24e2841f7e88af2b589cb3bc70b6621
Nimotomato <108477753+nimotomato@users.noreply.github.com>
2023-07-02 09:24:04 +0200

    updated projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,6 +2,7 @@ import github from "../img/github.jpg";
 import IITH from "../img/IITH.jpg";
 import pong from "../img/pong.jpg";
 import githubpong from "../img/githubpong.jpg";
+import stroop from "../img/stroop.jpg";
 
 const projectList = [
   {
@@ -12,22 +13,22 @@ const projectList = [
     altUrl: "https://github.com/nimotomato/isitgoingtohell-webpage-portfolio",
     id: 1,
   },
-  // {
-  //   url: "https://open.spotify.com/playlist/2iz1IxBJVzcmnOrPDxQfYj?si=2eef6e14a3304d56",
-  //   img: spotifyLongly,
-  //   text: "Spotify bot",
-  //   altImg: github,
-  //   altUrl: "https://github.com/nimotomato/DiscoverLongly",
-  //   id: 2,
-  // },
   {
-    url: "https://pong-ten-plum.vercel.app/",
-    img: pong,
-    text: "Pong single player",
-    altImg: githubpong,
-    altUrl: "https://github.com/nimotomato/pong-portfolio",
-    id: 3,
+    url: "https://stroop.vercel.app/",
+    img: stroop,
+    text: "Neuropsych test",
+    altImg: github,
+    altUrl: "https://github.com/nimotomato/stroop",
+    id: 2,
   },
+  // {
+  //   url: "https://pong-ten-plum.vercel.app/",
+  //   img: pong,
+  //   text: "Pong single player",
+  //   altImg: githubpong,
+  //   altUrl: "https://github.com/nimotomato/pong-portfolio",
+  //   id: 3,
+  // },
 ];
 
 export default projectList;
