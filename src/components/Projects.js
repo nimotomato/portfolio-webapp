f67ac16ae1a5481523f82418e5f0b034b4471ff7
Nimotomato <108477753+nimotomato@users.noreply.github.com>
2023-07-06 17:08:36 +0200

    updated projects css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,43 @@
 import '../css/Project.css';
-
+import { useState, useRef } from 'react';
 
 const Projects = (props) => {
-    const projects = props.projects;
+    const projectsRef = useRef(props.projects)
 
+    const [hovering, setIsHovering] = useState("")
 
     const handleClick = (url) => {
         window.open(url);
+    }
+
+    const handleOnMouseEnter = (projectId) => {
+        setIsHovering(projectId);
+    }
+
+    const handleOnMouseLeave = () => {
+        setIsHovering("");
     }
  
 
     return (
         <div className="projects-wrapper">
-            {projects.map((proj) => (
+            {projectsRef.current.map((proj) => (
+                
                 <div className="project" key={proj.id}>
+                    <div className='project-splitter'>
                     <img 
                     onClick={() => handleClick(proj.url)} 
-                    onMouseLeave={e => e.currentTarget.src = proj.img}
-                    onMouseEnter={e => e.currentTarget.src = proj.altImg}
+                    onMouseEnter={() => handleOnMouseEnter(proj.id)}
+                    onMouseLeave={() => handleOnMouseLeave()}
                     src={proj.img} 
                     alt="alt"/>
+                    {(hovering === proj.id) && <img 
+                    onClick={() => handleClick(proj.altUrl)} 
+                    onMouseEnter={() => handleOnMouseEnter(proj.id)}
+                    onMouseLeave={() => handleOnMouseLeave()}
+                    src={proj.altImg} 
+                    alt="alt"/>}
+                    </div>
                     <p className="project-text">
                         {proj.text}
                     </p>
