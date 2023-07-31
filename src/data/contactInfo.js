87530a018730cb84f7092d3d0ce6781d974dfdc2
nimotomato <108477753+nimotomato@users.noreply.github.com>
2023-07-31 16:19:25 +0200

    added github icon link

--- src/data/contactInfo.js
+++ src/data/contactInfo.js
@@ -1,6 +1,7 @@
 const contactInfo = {
     email: "martinjohansson36@protonmail.com",
-    linkedIn: "https://www.linkedin.com/in/martin-johansson-79a8a524a/"
+    linkedIn: "https://www.linkedin.com/in/martin-johansson-79a8a524a/",
+    github: "https://github.com/nimotomato"
 }
 
 export default contactInfo;
